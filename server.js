460a4ef1de629c09e2453677910af87ff6fa1558
bilel <bilelyaich16@gmial.com>
2025-11-30 21:28:21 +0100

    Ajout du dossier public et des images

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-origin: "[http://localhost:3000](http://localhost:3000)", 
+origin: 'maison-frontend-5att.vercel.app', 
 credentials: true,
 }));
 
