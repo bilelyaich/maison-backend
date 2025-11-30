6c9ed5e1b99d91ddb9b78dbe41212811aa4e06e8
bilel <bilelyaich16@gmial.com>
2025-11-30 20:42:53 +0100

    Ajout du dossier public et des images

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const connectDB = require('./config/db');
 const express = require('express');
 const authRoutes = require("./routes/authRoutes");
-const cors = require('cors'); // ✅ Importer cors
+const cors = require('cors'); 
 require('dotenv').config(); 
 
 const app = express();
 
-// Autoriser CORS
 app.use(cors({
-  origin: "http://localhost:3000", // ✅ Autorise uniquement ton frontend
-  credentials: true, // si tu utilises les cookies ou auth
+  origin: "http://localhost:3000", 
+  credentials: true, 
 }));
 
 app.use(express.json()); 
@@ -17,11 +16,8 @@ app.use(express.json());
 connectDB();
 const PORT = process.env.PORT || 5000;
 
-// Routes
 app.use("/api/users", authRoutes);
 
-// Test route
 app.get('/', (req, res) => res.send('Server is running!'));
 
-// Lancer le serveur
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
